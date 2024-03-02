947b131ff547766d1f61fe30f32177cba883f935
Zeeshan Mukhtar <Zeshanmukhtar0878@gmail.com>
2024-03-01 21:59:29 -0800

    🎉 begin a project

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline bg-slate-700 text-white mt-4 tracking-wider text-center mx-auto">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
