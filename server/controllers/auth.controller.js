a18c0e20367eebfce5c9c4a368ab361ddda6e860
Zeeshan Mukhtar <91063160+ZeeshanMukhtar1@users.noreply.github.com>
2024-03-04 19:58:10 +0500

    👽️ signin api

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -8,6 +10,32 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json({ message: 'User created successfully' });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, 'user not found in db'));
+
+    const validpassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validpassword) return next(errorHandler(401, 'invalid password'));
+    // at this stage user is valid, we wanna store something unique from that user (like username , email or _id , _id is best usage )and store it in the browser for future , on future  browser takes that credtilas and send it to server to verify the user
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    // we dont wanna send password to the client
+    const { password: hashedPassword, ...rest } = validUser._doc;
+    // console.log(validUser);
+    const expirydate = new Date(new Date().getTime() + 60 * 60 * 1000); // 1 hour
+    res
+      .cookie('access_token', token, {
+        httpOnly: true,
+        expires: expirydate,
+      })
+      .status(200)
+      .json({ ...rest, token });
   } catch (error) {
     next(error);
   }
