165c923bb4c67c5d7cba89b333438c2a69cccf84
Zeeshan Mukhtar <91063160+ZeeshanMukhtar1@users.noreply.github.com>
2024-03-08 19:00:15 +0500

    🔥 Refactor Profile component to improve file upload functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useRef, useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import { useRef, useState, useEffect } from 'react';
 import {
   getDownloadURL,
   getStorage,
@@ -8,131 +8,105 @@ import {
 } from 'firebase/storage';
 import { app } from '../firebase';
 
-function Profile() {
+export default function Profile() {
   const fileRef = useRef(null);
-  const { currentUser } = useSelector((state) => state.user);
   const [image, setImage] = useState(undefined);
-  const [imagePercentage, setImagePercentage] = useState(0);
+  const [imagePercent, setImagePercent] = useState(0);
   const [imageError, setImageError] = useState(false);
   const [formData, setFormData] = useState({});
 
+  const { currentUser } = useSelector((state) => state.user);
   useEffect(() => {
     if (image) {
       handleFileUpload(image);
     }
   }, [image]);
-
   const handleFileUpload = async (image) => {
     const storage = getStorage(app);
-    const fileName = new Date().getTime() + '-' + image.name;
+    const fileName = new Date().getTime() + image.name;
     const storageRef = ref(storage, fileName);
     const uploadTask = uploadBytesResumable(storageRef, image);
-
     uploadTask.on(
       'state_changed',
       (snapshot) => {
         const progress =
           (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        setImagePercentage(Math.round(progress));
-        switch (snapshot.state) {
-          case 'paused':
-            console.log('Upload is paused');
-            break;
-          case 'running':
-            console.log('Upload is running');
-            break;
-          default:
-            console.log('Upload is done');
-            break;
-        }
+        setImagePercent(Math.round(progress));
       },
       (error) => {
-        console.error(error);
         setImageError(true);
       },
       () => {
-        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-          setFormData({ ...formData, profilePicture: downloadURL });
-          setImagePercentage(100);
-        });
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+          setFormData({ ...formData, profilePicture: downloadURL })
+        );
       }
     );
   };
-
   return (
-    <div className="container p-8 mx-auto">
-      <h1 className="text-3xl font-semibold text-center mb-7">Your Profile</h1>
-
-      <div className="relative mb-5">
+    <div className="max-w-lg p-3 mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+      <form className="flex flex-col gap-4">
         <input
           type="file"
-          hidden
           ref={fileRef}
+          hidden
           accept="image/*"
           onChange={(e) => setImage(e.target.files[0])}
         />
+        {/* 
+      firebase storage rules:  
+      allow read;
+      allow write: if
+      request.resource.size < 2 * 1024 * 1024 &&
+      request.resource.contentType.matches('image/.*') */}
         <img
-          className="items-center object-cover w-20 h-20 border-2 border-gray-300 rounded-full cursor-pointer "
           src={formData.profilePicture || currentUser.profilePicture}
-          alt="profile picture"
+          alt="profile"
+          className="self-center object-cover w-24 h-24 mt-2 rounded-full cursor-pointer"
           onClick={() => fileRef.current.click()}
         />
-        {imageError && (
-          <span className="mt-2 text-red-500">
-            Error uploading image to Firebase
-          </span>
-        )}
-        {imagePercentage > 0 && imagePercentage < 100 && (
-          <span className="mt-2 text-red-500">
-            Image upload in progress... {imagePercentage}%
-          </span>
-        )}
-        {imagePercentage === 100 && (
-          <span className="mt-2 text-green-500">
-            Image uploaded successfully
-          </span>
-        )}
-      </div>
-
-      <form className="flex flex-col items-center w-full max-w-md mx-auto">
+        <p className="self-center text-sm">
+          {imageError ? (
+            <span className="text-red-700">
+              Error uploading image (file size must be less than 2 MB)
+            </span>
+          ) : imagePercent > 0 && imagePercent < 100 ? (
+            <span className="text-slate-700">{`Uploading: ${imagePercent} %`}</span>
+          ) : imagePercent === 100 ? (
+            <span className="text-green-700">Image uploaded successfully</span>
+          ) : (
+            ''
+          )}
+        </p>
         <input
+          defaultValue={currentUser.username}
           type="text"
           id="username"
           placeholder="Username"
-          className="p-2 mb-3 border border-gray-300 rounded"
-          value={currentUser.username}
+          className="p-3 rounded-lg bg-slate-100"
         />
         <input
-          type="text"
+          defaultValue={currentUser.email}
+          type="email"
           id="email"
           placeholder="Email"
-          className="p-2 mb-3 border border-gray-300 rounded"
-          value={currentUser.email}
+          className="p-3 rounded-lg bg-slate-100"
         />
         <input
           type="password"
           id="password"
           placeholder="Password"
-          className="p-2 mb-3 border border-gray-300 rounded"
+          className="p-3 rounded-lg bg-slate-100"
         />
-        <button
-          type="submit"
-          className="w-full p-2 mt-5 text-white bg-blue-500 rounded"
-        >
-          Update
+        <button className="p-3 text-white uppercase rounded-lg bg-slate-700 hover:opacity-95 disabled:opacity-80">
+          update
         </button>
       </form>
-
-      <div className="flex flex-col items-center mt-8">
-        <span className="mb-2 text-red-500 cursor-pointer hover:underline">
-          Delete Account
-        </span>
-        <span className="text-blue-500 cursor-pointer hover:underline">
-          Logout
-        </span>
+      <div className="flex justify-between mt-5">
+        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <span className="text-red-700 cursor-pointer">Sign out</span>
       </div>
     </div>
   );
 }
-
-export default Profile;
