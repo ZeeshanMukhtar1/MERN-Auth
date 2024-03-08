af28db3bdf5ac6a086698a639afc1edd1a0b00b5
Zeeshan Mukhtar <91063160+ZeeshanMukhtar1@users.noreply.github.com>
2024-03-08 21:47:49 +0500

    🔥 Add delete account functionality

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -38,5 +38,18 @@ export const updateUser = async (req, res, next) => {
     res.status(200).json(rest);
   } catch (error) {
     next(error);
+    console.log(error);
+  }
+};
+
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id) {
+    return next(errorHandler(401, 'You can delete only your account!'));
+  }
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json('User has been deleted...');
+  } catch (error) {
+    next(error);
   }
 };
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,10 +1,15 @@
 import express from 'express';
-import { test, updateUser } from '../controllers/user.controller.js';
+import {
+  test,
+  updateUser,
+  deleteUser,
+} from '../controllers/user.controller.js';
 import { verifyUser } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.get('/', test);
 router.post('/update/:id', verifyUser, updateUser);
+router.delete('/delete/:id', verifyUser, deleteUser);
 
 export default router;
