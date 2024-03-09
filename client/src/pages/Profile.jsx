652be06bbbd290833206d220a8523bcf6ee05350
Zeeshan Mukhtar <91063160+ZeeshanMukhtar1@users.noreply.github.com>
2024-03-09 16:40:13 +0500

    Update profile page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -166,7 +166,7 @@ export default function Profile() {
           onChange={handleChange}
         />
         <button
-          className="p-3 text-white uppercase rounded-lg bg-slate-700 hover:bg-slate-800 disabled:opacity-80 focus:outline-none"
+          className="p-3 text-white uppercase rounded-lg bg-green-700 hover:bg-green-800 disabled:opacity-80 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 cursor-pointer"
           disabled={loading}
         >
           {loading ? 'Loading...' : 'Update'}
@@ -175,13 +175,13 @@ export default function Profile() {
       <div className="flex justify-between mt-5">
         <span
           onClick={handleDeleteAccount}
-          className="text-red-700 cursor-pointer hover:underline"
+          className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 cursor-pointer"
         >
           Delete Account
         </span>
         <span
           onClick={handleSignout}
-          className="text-red-700 cursor-pointer hover:underline"
+          className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 cursor-pointer"
         >
           Sign out
         </span>
